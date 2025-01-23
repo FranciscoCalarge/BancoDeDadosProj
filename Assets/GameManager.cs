b47784c8d15995f8dc52e7c642f0d6908d9acd56
Francisco Calarge <fjcalarge@gmail.com>
2025-01-22 21:45:25 -0300

    safety commit

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,26 +5,25 @@ using Mono.Data.Sqlite;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject chaveiroPrefab;
     IDbConnection conexaoDb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         conexaoDb = CriarEAbrirDB();
         IDbCommand comandoCriarTabela = conexaoDb.CreateCommand();
-        comandoCriarTabela.CommandText = "CREATE TABLE IF NOT EXISTS SACOLA (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                                                                            "nome VARCHAR(255), " +
-                                                                            "quantidade INTEGER, " +
-                                                                            "tipo VARCHAR(255), " +
-                                                                            "descricao VARCHAR(255)  )";
+        comandoCriarTabela.CommandText = 
+        "CREATE TABLE IF NOT EXISTS SACOLA (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                           "nome VARCHAR(255), " +
+                                           "quantidade INTEGER, " +
+                                           "tipo VARCHAR(255), " +
+                                           "descricao VARCHAR(255)  )";
+        ComandoCarregarTabela("SACOLA");
+
 
         comandoCriarTabela.ExecuteReader();
-        ComandoInserirTabela("panela",2,"defesa","dois");
-        ComandoInserirTabela("teste",3,"defesa","dois");
-        ComandoInserirTabela("maconha",4,"senho","dois");
-        ComandoInserirTabela("ata",5,"defesa","jisuis");
         
         conexaoDb.Close();
-        Debug.Log("Closed");
     }
 
     public void ComandoInserirTabela(string nome_item, int quantidade_item, string tipo_item, string descricao_item)
@@ -32,6 +31,27 @@ public class GameManager : MonoBehaviour
         IDbCommand comandoInserir = conexaoDb.CreateCommand();
         comandoInserir.CommandText = String.Format("INSERT INTO SACOLA (id,nome,quantidade,tipo,descricao) VALUES (null,'{0}','{1}' ,'{2}','{3}')", nome_item,quantidade_item,tipo_item,descricao_item);
         comandoInserir.ExecuteNonQuery();
+
+    }
+
+
+    public void ComandoCarregarTabela(string tabela)
+    {
+        IDbCommand comandoSelecionar = conexaoDb.CreateCommand();
+        comandoSelecionar.CommandText = String.Format("SELECT * FROM {0}",tabela);
+
+        using (IDataReader reader = comandoSelecionar.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                GameObject aux =  Instantiate(chaveiroPrefab);
+                aux.name =reader.GetString(1);
+                SpringJoint joint = aux.GetComponent<SpringJoint>();
+                joint.connectedBody = GameObject.Find("Sphere").GetComponent<Rigidbody>();
+                joint.tolerance = .5f;
+            }
+        }
+
 
     }
 
