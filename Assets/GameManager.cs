c0f1b6f27b9d279e1d0ac2e9a60f8bfc310389c9
Francisco Calarge <fjcalarge@gmail.com>
2025-01-22 22:05:15 -0300

    safe commit

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,25 +11,34 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         conexaoDb = CriarEAbrirDB();
+
+        ComandoCriarTabela();
+
+        ComandoCarregarTabela("SACOLA");
+       
+        conexaoDb.Close();
+    }
+
+    public void ComandoCriarTabela()
+    {
         IDbCommand comandoCriarTabela = conexaoDb.CreateCommand();
-        comandoCriarTabela.CommandText = 
+        comandoCriarTabela.CommandText =
         "CREATE TABLE IF NOT EXISTS SACOLA (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                                            "nome VARCHAR(255), " +
                                            "quantidade INTEGER, " +
                                            "tipo VARCHAR(255), " +
                                            "descricao VARCHAR(255)  )";
-        ComandoCarregarTabela("SACOLA");
 
 
         comandoCriarTabela.ExecuteReader();
-        
-        conexaoDb.Close();
     }
 
     public void ComandoInserirTabela(string nome_item, int quantidade_item, string tipo_item, string descricao_item)
     {
         IDbCommand comandoInserir = conexaoDb.CreateCommand();
-        comandoInserir.CommandText = String.Format("INSERT INTO SACOLA (id,nome,quantidade,tipo,descricao) VALUES (null,'{0}','{1}' ,'{2}','{3}')", nome_item,quantidade_item,tipo_item,descricao_item);
+        comandoInserir.CommandText = 
+        String.Format("INSERT INTO SACOLA (id,nome,quantidade,tipo,descricao) VALUES (null,'{0}','{1}' ,'{2}','{3}')",
+                                             nome_item,quantidade_item,tipo_item,descricao_item);
         comandoInserir.ExecuteNonQuery();
 
     }
@@ -48,10 +57,10 @@ public class GameManager : MonoBehaviour
                 aux.name =reader.GetString(1);
                 SpringJoint joint = aux.GetComponent<SpringJoint>();
                 joint.connectedBody = GameObject.Find("Sphere").GetComponent<Rigidbody>();
-                joint.tolerance = .5f;
+                aux.GetComponent<MeshRenderer>().material = aux.GetComponent<MeshRenderer>().material;
+                aux.GetComponent<MeshRenderer>().material.SetFloat("_hue_offset", UnityEngine.Random.value);
             }
         }
-
 
     }
 
